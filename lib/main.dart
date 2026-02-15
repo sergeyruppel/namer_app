f89967b62415916a79d17d1a2590932e45349b62
Sergey Ruppel <sergeyruppel@users.noreply.github.com>
2026-02-15 20:00:03 +0700

    Add favorites functionality

--- lib/main.dart
+++ lib/main.dart
@@ -26,8 +26,20 @@ class MyApp extends StatelessWidget {
 
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
+
   void getNext() {
     current = WordPair.random();
+    notifyListeners();
+  }
+
+  var favorites = <WordPair>[];
+
+  void toggleFavorite() {
+    if (favorites.contains(current)) {
+      favorites.remove(current);
+    } else {
+      favorites.add(current);
+    }
     notifyListeners();
   }
 }
